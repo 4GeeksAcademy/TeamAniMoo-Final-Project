2286eb3ca5a1eebb330e2c9e5fa23903c73ca40d
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-08-29 00:39:09 +0000

    cards api

--- src/front/js/component/articles.js
+++ src/front/js/component/articles.js
@@ -9,19 +9,20 @@ const Articles = () => {
 
 	const conditionalRender = () => {
 		if(!isSearch){
-			return popularAnime.map((anime) => {
+      const topFiveAnime = popularAnime.slice(0,5);
+			return topFiveAnime.map((anime) => {
 				return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
           
-                <div className="container">
+          <div className="container">
                 <div className="cards">
-                  <div className="row row-cols-1 row-cols-md-3 g-4">
-                    <div className="col-md-4">
+                  <div className="row card_row">
+                    <div className="col-md-3 img_container">
                       <img className="imageGo" src={anime.images.jpg.image_url} />
                     </div>
                     <div className="col-md-8">
                       <div className="card-body">
-                        <h4 className="card-title text-center">{anime.title_english}</h4>
-                        <h4 className ="card-title text-center">Rank : {anime.popularity}</h4>
+                        <h4 className="card-title text-start">{anime.title_english}</h4>
+                        <h4 className ="card-title text-start">Rank: {anime.popularity}</h4>
                         <p className="card-text">
                           {anime.synopsis}
                         </p>
